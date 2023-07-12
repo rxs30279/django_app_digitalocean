efab40ca5d386bab5d3be78709d748f5563247f2
Richard <richard_stephens@hotmail.co.uk>
2023-07-12 15:56:43 +0100

    changed debug

--- django_app/settings.py
+++ django_app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config("DEBUG", True)
 
 
 # ALLOWED_HOSTS = ("orca-app-8r5xc.ondigitalocean.app", "127.0.0.1", ".localhost")
